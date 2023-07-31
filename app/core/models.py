3c4bd63522826924718a1d7dc22fef178accf890
Dexter Ryser <redtex2310@gmail.com>
2023-07-31 11:19:41 -0400

    Adding admin list test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    # extrafields enables us to provide keyword args
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save amd return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # Determines who can login as admin
+    is_staff = models.BooleanField(default=True)
+
+    # Assign to user manager
+    objects = UserManager()
+    # Replaces default username field that comes with the
+    # user model
+    USERNAME_FIELD = 'email'
